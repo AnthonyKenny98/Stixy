808fa4d7aae58503b36524de627f7eaba5edf5b8
AnthonyKenny98 <anthony_kenny@college.harvard.edu>
2020-05-12 20:37:45 +1000

    Colorize test output:

--- stixy/accounting/tests.py
+++ stixy/accounting/tests.py
@@ -4,22 +4,19 @@ from django.test import TestCase
 from .models import AccountClass, AccountGroup
 
 
-class AssetClassModelTests(TestCase):
-    """Test AssetClass Model."""
+# Init Test Instances
+asset = AccountClass(name='asset', positive_entry='debit')
+current_asset = AccountGroup(name='current asset', account_class=asset)
 
-    def test_is_asset(self):
-        """is_asset() returns true if assetclass is "asset"."""
-        a = AccountClass(name='Asset', positive_entry='debit')
-        self.assertIs(a.is_asset(), True)
+class AccountClassTests(TestCase):
+    """Test AccountClass Model."""
+
+    def test_name(self):
+        self.assertEqual(asset.name, 'asset')
 
 
 class AssetGroupModelTests(TestCase):
     """Test AssetGroup Model."""
 
-    def test_asset_is_debit(self):
-        g = AccountGroup(
-            name='Current Assets',
-            account_class=AccountClass(
-                name='Asset',
-                positive_entry='debit'))
-        self.assertIs(g.account_class.positive_entry, 'debit')
+    def test_name(self):
+        self.assertEqual(current_asset.name, 'current asset')
